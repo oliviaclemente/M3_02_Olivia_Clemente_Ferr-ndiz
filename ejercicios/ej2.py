25632b6d5b87f4b5a3faa98603f1c979d1fcb35e
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-11-20 15:57:03 +0000

    cc

--- ejercicios/ej2.py
+++ ejercicios/ej2.py
@@ -1,3 +1,7 @@
 lista=[4, 7, 30, 23, 5]
 def error(lista):
-  
+  try:
+    lista[10]
+  except:
+    print("Error")
+print(error(lista))
--- main.py
+++
@@ -1,7 +0,0 @@
-def error(x,y):
-  try:
-    x/y
-  except:
-     print("Imposible")
-
-print(error(7,0))
